1455eed5dcfdc014924ff63e22905a599670b04f
herisusantoarismann <heri.s.arisman@gmail.com>
2021-08-30 10:03:37 +0700

    Add Loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div v-if="data === ''"><p>KOSONG</p></div>
+  <div v-if="data === ''">
+    <div class="bg-loader">
+      <div class="loader">
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+      </div>
+    </div>
+  </div>
   <div class="container" :class="bg" v-else>
     <div class="leftBox">
       <h1>Wheater App</h1>
@@ -134,6 +146,7 @@ export default {
 .container .leftBox h1 {
   font-size: 1.3rem;
   font-weight: 700;
+  cursor: pointer;
 }
 .container .leftBox .wheater {
   width: 60%;
@@ -318,6 +331,64 @@ export default {
   }
   .container .leftBox .wheater > img {
     width: 50px;
+  }
+}
+.bg-loader {
+  background: rgb(212, 194, 194);
+  min-height: 100vh;
+  min-width: 100vw;
+}
+.loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
+.loader span {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 30px;
+  color: aliceblue;
+  display: inline-block;
+  transition: all 0.5s;
+  animation: animate 2s infinite;
+}
+.loader span:nth-child(1) {
+  animation-delay: 0.1s;
+}
+.loader span:nth-child(2) {
+  animation-delay: 0.2s;
+}
+.loader span:nth-child(3) {
+  animation-delay: 0.3s;
+}
+.loader span:nth-child(4) {
+  animation-delay: 0.4s;
+}
+.loader span:nth-child(5) {
+  animation-delay: 0.5s;
+}
+.loader span:nth-child(6) {
+  animation-delay: 0.6s;
+}
+.loader span:nth-child(7) {
+  animation-delay: 0.7s;
+}
+@keyframes animate {
+  0% {
+    color: aliceblue;
+    transform: translateY(0);
+    margin-left: 0;
+  }
+  25% {
+    color: aliceblue;
+    transform: translateY(-15px);
+    margin-left: 10px;
+    text-shadow: 0 15px 5px rgba(0, 0, 0, 1);
+  }
+  100% {
+    color: aliceblue;
+    transform: translateY(0);
+    margin-left: 0;
   }
 }
 </style>
